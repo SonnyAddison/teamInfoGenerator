e333e9c5ad2d2c966b03399a533872645a64e347
SonnyAddison <101370278+SonnyAddison@users.noreply.github.com>
2022-07-18 23:32:30 -0700

    updaing the work put in on getting HTML to look right and add in questions

---
+++ assets/utils/generateHTML.js
@@ -0,0 +1,70 @@
+// Building out the HTML for the page
+
+generateHTML= (data) => {
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset='UTF-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <meta http-equiv='X-UA-Compatible' content='ie=edge'>
+        <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossorigin='anonymous'>
+        
+        
+
+        <title>Employee Tracker</title>
+    </head>
+    <body>
+        <div class='jumbotron jumbotron-fluid p-3 mb-2 bg-primary text-white">.bg-primary '>
+            
+                <h1 class='display-1 text-center'>My Team</h1>
+            
+        </div>
+        <div class='container d-flex justify-content-around'>
+            <div class='card' style='width: 18rem;'>
+                <div class='card-header bg-success p-2 text-white bg-opacity-75 text-center' id='manager'>
+                    <h1>${data.name}</h1>
+                    <h2>Manager</h2>
+                </div>
+                <ul class='list-group list-group-flush'>
+                    <li class='list-group-item'><button type='button' class='btn btn-secondary' style='width: 100%'>ID: ${data.id}</button></li>
+                    <li class='list-group-item'><button type='button' class='btn btn-secondary' style='width: 100%'>Email: ${data.email}</button></li>
+                    <li class='list-group-item'><button type='button' class='btn btn-secondary' style='width: 100%'>Office Number: ${data.officeNumber}</button></li>
+                </ul> 
+            </div>
+            <div class='card' style='width: 18rem;'>
+            <div class='card-header bg-success p-2 text-white bg-opacity-75 text-center' id='engineer'>
+                <h1>${data.enName}</h1>
+                <h2>Engineer</h2>
+            </div>
+                <ul class='list-group list-group-flush'>
+                    <li class='list-group-item'><button type='button' class='btn btn-secondary' style='width: 100%'>ID: ${data.id}</button></li>
+                    <li class='list-group-item'><button type='button' class='btn btn-secondary' style='width: 100%'>Email: ${data.enEmail}</button></li>
+                    <li class='list-group-item'><button type='button' class='btn btn-secondary' style='width: 100%'>Github: ${data.github}</button></li>
+                </ul>
+            </div>
+            <div class='card' style='width: 18rem;'>
+                <div class='card-header bg-success p-2 text-white bg-opacity-75 text-center' id='intern'>
+                    <h1>${data.inName}</h1>
+                    <h2>Intern</h2>
+                </div>
+            <ul class='list-group list-group-flush'>         
+                <li class='list-group-item'><button type='button' class='btn btn-secondary' style='width: 100%'>ID: ${data.id}</button></li>
+                <li class='list-group-item'><button type='button' class='btn btn-secondary' style='width: 100%'>Email: ${data.inEmail}</button></li>
+                <li class='list-group-item'><button type='button' class='btn btn-secondary' style='width: 100%'>School: ${data.school}</button></li>
+            </ul>
+        </div>
+    <script src='https://code.jquery.com/jquery-3.3.1.slim.min.js' integrity='sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo' crossorigin='anonymous'></script>
+    </body>
+</html>`
+}
+
+//To export the generateHTML function
+module.exports = generateHTML;
+
+
+
+    
+        
+
+
+
--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 //Import mode packages and files
-const inquirer = require('inquirer');
-const fs = require('fs');
-const util = require('util');
-const questions = require('./assets/qusetionsAsked');
-const generateHTML = require('./assets/generateHTML');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const generateHTML = require("./assets/utils/generateHTML");
+const questions = require('./assets/utils/questions');
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
 //fucntiont to inintat the app
-const init = async () => {
+const init = async() => {
     try {
-        const relpy = await inquirer.prompt(questions);
-        let readMeInfo = generateHTML(relpy);
-        await writeFileAsync('./index.html', readMeInfo);
-    } catch (err) {
-        console.log(err);
+        const reply = await inquirer.prompt(questions);
+        let readMeInfo = generateHTML(reply);
+        await writeFileAsync("index.html", readMeInfo);
+        console.log("Successfully wrote to index.html");   
+        } catch (err) {
+    console.log(err);
     }
 }
+
 
 //function to start the app
 init();
