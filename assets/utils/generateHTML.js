d0041153c6a76a9348416e4c4f69e7f4895505b3
SonnyAddison <101370278+SonnyAddison@users.noreply.github.com>
2022-07-19 10:10:50 -0700

    deleted index html page that was test run

--- assets/utils/generateHTML.js
+++ assets/utils/generateHTML.js
@@ -52,6 +52,7 @@ generateHTML= (data) => {
                 <li class='list-group-item'><button type='button' class='btn btn-secondary' style='width: 100%'>Email: ${data.inEmail}</button></li>
                 <li class='list-group-item'><button type='button' class='btn btn-secondary' style='width: 100%'>School: ${data.school}</button></li>
             </ul>
+            
         </div>
     <script src='https://code.jquery.com/jquery-3.3.1.slim.min.js' integrity='sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo' crossorigin='anonymous'></script>
     </body>
